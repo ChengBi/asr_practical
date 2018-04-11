e854d3f34ac329d17a04fbb18c60dbdf54a8d1b9
Bi, Cheng <cheng.bi@nuance.com>
2018-04-11 18:06:07 +0800

    Update

--- include/array.h
+++ include/array.h
@@ -10,7 +10,7 @@ namespace asr {
 
 	template <class Elem> class Array {
 	public:
-		virtual Elem& at(const int) const = 0;
+		virtual Elem at(const int) const = 0;
 		virtual int size() const = 0;
 		virtual void clear() = 0;
 		virtual std::string tostring() const = 0;
@@ -20,21 +20,21 @@ namespace asr {
 	private:
 		void deleteall();
 		int length;
-		Elem* value;
 		int tail_index;
+		Elem* value;
 	public:
 		Scalar(const int);
 		Scalar();
 		~Scalar();
-		Elem& at(const int) const;
 		int size() const;
 		void clear();
-		std::string tostring() const;
 		bool isFull() const;
-		Scalar<Elem> operator+(const Scalar<Elem>&);
+		Elem at(const int) const;
+		std::string tostring() const;
 		void operator << (Elem);
-		// Elem& operator [] (const int);
-		Scalar<Elem> operator = (const Scalar<Elem>&);
+		Elem& operator [] (const int);
+		Scalar<Elem>& operator + (const Scalar<Elem>&) const;
+		Scalar<Elem>& operator = (const Scalar<Elem>&);
 	};
 }
 
@@ -44,11 +44,6 @@ template <class Elem> void asr::Scalar<Elem>::deleteall() {
 	this->length = 0;
 	this->tail_index = 0;
 }
-// template <class Elem> asr::Scalar<Elem>::Scalar() {
-// 	this->length = 0;
-// 	this->value = new Elem[0];
-// 	this->tail_index = 0;
-// }
 
 template <class Elem> asr::Scalar<Elem>::Scalar(const int n) {
 	this->length = n;
@@ -63,7 +58,7 @@ template <class Elem> asr::Scalar<Elem>::~Scalar() {
 	this->deleteall();
 }
 
-template <class Elem> Elem& asr::Scalar<Elem>::at(const int index) const {
+template <class Elem> Elem asr::Scalar<Elem>::at(const int index) const {
 	try {
 		if (index < 0 || index >= this->size()) {
 			throw asr::IndexExceedException();
@@ -82,24 +77,17 @@ template <class Elem> int asr::Scalar<Elem>::size() const {
 }
 
 template <class Elem> void asr::Scalar<Elem>::clear() {
-	// std::cout << "clear" << std::endl;
 	this->deleteall();
 	this->value = new Elem[this->size()];
 }
 
 template <class Elem> std::string asr::Scalar<Elem>::tostring() const {
 	std::string str = "Value is: ";
-	// std::string temp = "";
-	// std::cout << this->size() << std::endl;
 	for (int i = 0; i < this->size(); i ++) {
 		std::stringstream ss;
 		ss << this->value[i];
-		// ss >> temp;
 		str += ss.str() + " ";
-		// ss.clear();
-		// temp = "";
 	}
-	// ss.close();
 	return str;
 }
 template <class Elem> bool asr::Scalar<Elem>::isFull() const {
@@ -108,18 +96,16 @@ template <class Elem> bool asr::Scalar<Elem>::isFull() const {
 	return false;
 }
 
-template <class Elem> asr::Scalar<Elem> asr::Scalar<Elem>::operator + (const asr::Scalar<Elem>& b) {
+template <class Elem> asr::Scalar<Elem>& asr::Scalar<Elem>::operator + (const asr::Scalar<Elem>& b) const {
 	try {
 		if ((this->size() != b.size()) || !this->isFull() || !b.isFull()) {
 			throw asr::ArraySizeMismatchException();
 		} else {
-			asr::Scalar<Elem> res(this->size());
+			asr::Scalar<Elem>* res = new asr::Scalar<Elem>(this->size());
 			for (int i = 0; i < this->size(); i ++) {
-				res.at(i) = b.at(i) + this->at(i);
-				// res.at(i)
+				(*res)[i] = b.at(i) + this->at(i);
 			}
-			std::cout << "\033[0;31;43m" << res.tostring() << " size: " << res.size() << " \033[0m" << std::endl;
-			return res;
+			return *res;
 		}
 		// printf("here1");
 	} catch (asr::ArraySizeMismatchException& e) {
@@ -133,23 +119,16 @@ template <class Elem> void asr::Scalar<Elem>::operator << (Elem value) {
 	if (this->tail_index == this->size()) {
 		printf("This scalar is full, please clear it first if you want to add elements!\n");
 	} else {
-		// std::cout <<this->tail_index << " => " << value << std::endl;
 		this->value[this->tail_index] = value;
 		this->tail_index += 1;
 	}
 }
 
-template <class Elem> asr::Scalar<Elem> asr::Scalar<Elem>::operator = (const asr::Scalar<Elem>& element) {
+template <class Elem> asr::Scalar<Elem>& asr::Scalar<Elem>::operator = (const asr::Scalar<Elem>& element) {
 	if (this->size() != element.size()) {
-
-		std::cout << "equal" << std::endl;
 		this->deleteall();
-		// std::cout << element.size() << std::endl;
-
 		this->value = new Elem[element.size()];
 		this->length = element.size();
-		// std::cout << this->size() << std::endl;
-
 	}
 	for (int i = 0; i< this->size(); i ++ ) {
 		this->value[i] = element.at(i);
@@ -157,8 +136,17 @@ template <class Elem> asr::Scalar<Elem> asr::Scalar<Elem>::operator = (const asr
 	return *this;
 }
 
-// template <class Elem> Elem& asr::Scalar<Elem>::operator [] (const int index) {
-// 	return this->at(index);
-// }
+template <class Elem> Elem& asr::Scalar<Elem>::operator [] (const int index) {
+	try {
+		if (index < 0 || index >= this->size()) {
+			throw asr::IndexExceedException();
+		} else {
+			return this->value[index];
+		}
+	} catch (asr::IndexExceedException& e) {
+		printf("\033[0;31;43m%s\033[0m\n", e.what());
+		exit(-1);
+	}
+}
 
 #endif
--- main.cc
+++ main.cc
@@ -6,26 +6,25 @@
 int main() {
 
 
+
 	asr::Scalar<float> s(10);
 	asr::Scalar<float> s1(10);
-	// asr::Scalar<float> s2(10);
+	asr::Scalar<float> s2(10);
+	// ;
 
 	for (int i = 0; i < 15; i ++) {
 		s << i*1.0;
 		s1 << i*1.0 + 2;
 	}
-
 	std::cout << s.tostring() << std::endl;
 	std::cout << s1.tostring() << std::endl;
+	std::cout << s2.tostring() << std::endl;
 
-	// asr::Scalar<float> temp(10);// = s1 + s1;
-	// // s + s;
-	// s2 = s + s1;
-	s1 = s;
-	// std::cout << s.tostring() << std::endl;
+	s = s + s;
+	s2 = s + s1;
+	std::cout << s.tostring() << std::endl;
 	std::cout << s1.tostring() << std::endl;
-	// std::cout << (s + s1).tostring() << std::endl;
-	// std::cout << s2.tostring() << std::endl;
+	std::cout << s2.tostring() << std::endl;
 
 	// s1.at(5) = s.at(5) + s.at(5);
 	// std::cout << s1.at(5) << std::endl;
